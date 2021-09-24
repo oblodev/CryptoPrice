af913a96d2c6bcbaec96b009c85a6e5462f4e94d
Dawid Kostka <dpkostka@gmail.com>
2021-09-24 22:19:56 +0200

    added new api due to js restr.

--- js/app.js
+++ js/app.js
@@ -1,35 +1,41 @@
 const cryptoTableData = document.getElementById("crypto");
 const showMarketCap = document.getElementById("marketcap");
+const showMarketCapChange = document.getElementById("marketcap-change");
 
-function getCryptoLogo() {
-  fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/info`, {
-    method: "GET",
-    headers: {
-      "X-CMC_PRO_API_KEY": "32170971-f51f-4e5b-9cc3-aabb55b197be",
-    },
-  })
+function getCryptoMarketCap() {
+  fetch(`https://api.coingecko.com/api/v3/global`)
     .then((response) => response.json())
-    .then((data2) => {
-      console.log(data2.data);
+    .then((data) => {
+      const marketCap = data.data.total_market_cap.usd;
+      const marketCapChange = data.data.market_cap_change_percentage_24h_usd;
+
+      const formatter = new Intl.NumberFormat("en");
+      const formatedMarketCap = formatter.format(marketCap);
+      const formatedMarketCapChange = formatter.format(marketCapChange);
+      showMarketCap.textContent = `${formatedMarketCap} T`;
+      showMarketCapChange.textContent = ` ${formatedMarketCapChange}% `;
+
+      if (formatedMarketCapChange < 0) {
+        showMarketCapChange.classList.add("red");
+      } else if (formatedMarketCapChange > 0) {
+        showMarketCapChange.classList.add("green");
+      }
     })
     .catch((err) => {
-      console.log(err);
+      console.error(err);
     });
 }
 
-getCryptoLogo();
+getCryptoMarketCap();
 
 function getCryptoData() {
-  fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`, {
-    method: "GET",
-    headers: {
-      "X-CMC_PRO_API_KEY": "32170971-f51f-4e5b-9cc3-aabb55b197be",
-    },
-  })
+  fetch(
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d`
+  )
     .then((response) => response.json())
     .then((data) => {
-      cryptoData = data.data;
-      console.log(data.data);
+      //console.log(data);
+      const cryptoData = data;
 
       getData(cryptoData);
     })
@@ -40,66 +46,63 @@ function getCryptoData() {
 
 getCryptoData();
 
-function getCryptoMarketCap() {
-  fetch(`https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest`, {
-    method: "GET",
-    headers: {
-      "X-CMC_PRO_API_KEY": "32170971-f51f-4e5b-9cc3-aabb55b197be",
-    },
-  })
-    .then((response) => response.json())
-    .then((data1) => {
-      const marketCap = data1.data.quote.USD.total_market_cap.toFixed(2);
-      const formatter = new Intl.NumberFormat("en");
-      const formatedMarketCap = formatter.format(marketCap);
-      /*
-      function numberWithCommas(x) {
-        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-      }
-      const showCap = numberWithCommas(marketCap);
-      */
-      showMarketCap.textContent = `${formatedMarketCap}T`;
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-}
-
-getCryptoMarketCap();
-
 function getData(cryptoPriceData) {
   let data = "";
   cryptoPriceData.map((values) => {
     //console.log(values);
-    //console.log(values.quote.USD.price);
-    if (values.quote.USD.percent_change_24h < 0) {
-      color = "red";
-    } else if (values.quote.USD.percent_change_24h > 0) {
-      color = "green";
+
+    if (values.price_change_percentage_24h < 0) {
+      color24 = "red";
+    } else if (values.price_change_percentage_24h > 0) {
+      color24 = "green";
     } else {
-      color = "";
+      color24 = "";
     }
+
+    if (values.price_change_percentage_1h_in_currency < 0) {
+      color1H = "red";
+    } else if (values.price_change_percentage_1h_in_currency > 0) {
+      color1H = "green";
+    } else {
+      color1H = "";
+    }
+
+    if (values.price_change_percentage_7d_in_currency < 0) {
+      color7D = "red";
+    } else if (values.price_change_percentage_7d_in_currency > 0) {
+      color7D = "green";
+    } else {
+      color7D = "";
+    }
+
     const formatter = new Intl.NumberFormat("en");
-    const cryptoPrice = values.quote.USD.price.toFixed(2);
-    const marketCap = values.quote.USD.market_cap.toFixed(0);
-    const volume24 = values.quote.USD.volume_24h.toFixed(0);
+    const cryptoPrice = values.current_price;
+    const marketCap = values.market_cap;
+    const volume24 = values.total_volume;
     const formattedCryptoPrice = formatter.format(cryptoPrice);
     const formattedMarketCap = formatter.format(marketCap);
     const formattedVolume24 = formatter.format(volume24);
 
+    const cryptoLogo = values.image;
+
     data += `
         <tr>
-          <th cope="row">${values.cmc_rank} </th>
-          <td>${values.name} (${values.symbol})</td>
-          <td class="table__t-a-r"> $${formattedCryptoPrice}</td>
-          <td class="${color} table__t-a-r">${values.quote.USD.percent_change_24h.toFixed(
+          <th cope="row" class="p-16 bold">${values.market_cap_rank} </th>
+          <td class="p-16 bold"> <img src="${cryptoLogo}" class="img-size"> ${
+      values.name
+    } (${values.symbol.toUpperCase()})</td>
+          <td class="table__t-a-r p-16"> $${formattedCryptoPrice}</td>
+          <td class="${color1H} table__t-a-r p-16">${values.price_change_percentage_1h_in_currency.toFixed(
       2
     )}%</td>
-    <td class="${color} table__t-a-r">${values.quote.USD.percent_change_7d.toFixed(
+          <td class="${color24} table__t-a-r p-16">${values.price_change_percentage_24h.toFixed(
       2
     )}%</td>
-          <td class="table__t-a-r">$${formattedMarketCap}</td>
-          <td class="table__t-a-r">$${formattedVolume24}</td>
+          <td class="${color7D} table__t-a-r p-16">${values.price_change_percentage_7d_in_currency.toFixed(
+      2
+    )}%</td>
+          <td class="table__t-a-r p-16">$${formattedVolume24}</td>
+          <td class="table__t-a-r p-16">$${formattedMarketCap}</td>
         </tr>
         `;
   });

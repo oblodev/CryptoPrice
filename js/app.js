20e5c27198c483cf94a8a1c809f4f1754e5f7214
Dawid Kostka <dpkostka@gmail.com>
2021-10-19 15:22:54 +0200

    added sticky nav with js

--- js/app.js
+++ js/app.js
@@ -283,3 +283,22 @@ euros.addEventListener("click", function () {
     setInterval(newStart, 20000);
   });
 });
+
+// Sticky Nav
+const header = document.querySelector(".header");
+const obsCrypto = document.querySelector(".obsCrypto");
+//const navHeight = obsCrypto.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) header.classList.add("sticky");
+  else header.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+});
+headerObserver.observe(obsCrypto);
